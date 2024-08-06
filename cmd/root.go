adbcf427385a1a4501c3f214c883d2df5cc558ab
flvffywvffy <marcus@marcusleonasharvey.com>
2024-08-07 00:26:39 +0100

    updated version

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.0.1"
+const VERSION = "0.0.2"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
